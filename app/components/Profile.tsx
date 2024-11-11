992f8fb64b7f236ba8294c3ffb898ecab76d77c0
kimys-kor <ys2life1@naver.com>
2024-11-11 19:13:05 +0000

    로그아웃 홈이동 제거

--- app/components/Profile.tsx
+++ app/components/Profile.tsx
@@ -1,9 +1,7 @@
 "use client";
-
 import React from "react";
 import Avatar from "@/app/components/Avatar";
 import { removeCookie } from "../api/authAction";
-import { useAuthStore } from "@/app/globalStatus/useAuthStore";
 import { useUserStore } from "@/app/globalStatus/useUserStore";
 import Link from "next/link";
 import { UserInfo } from "../types";
@@ -13,14 +11,11 @@ interface ProfileProps {
 }
 
 function Profile({ userInfo }: ProfileProps) {
-  const { setLoggedIn } = useAuthStore();
   const { clearUserInfo } = useUserStore();
 
   const logoutSubmit = () => {
     removeCookie();
-    setLoggedIn(false);
     clearUserInfo();
-    window.location.href = "/"
   };
 
   return (
@@ -28,26 +23,17 @@ function Profile({ userInfo }: ProfileProps) {
       <Avatar />
       <h1 className="text-xl font-medium">{userInfo?.nickname || "Guest"}</h1>
       <div>
-        이름:{" "}
-        <span className="text-base text-black font-medium">
-          {userInfo?.fullName || "Unknown"}
-        </span>
+        이름: <span className="text-base text-black font-medium">{userInfo?.fullName || "Unknown"}</span>
       </div>
       <div>
-        포인트:{" "}
-        <span className="text-base text-blue font-medium">
-          {userInfo?.point ?? 0}
-        </span>
+        포인트: <span className="text-base text-blue font-medium">{userInfo?.point ?? 0}</span>
       </div>
       <Link href="/myinfo" className="cursor-pointer hover:text-blue">
         정보수정
       </Link>
 
       {userInfo?.sck && (
-        <Link
-          href="/protectedadmins"
-          className="cursor-pointer text-blue hover:text-deepblue"
-        >
+        <Link href="/protectedadmins" className="cursor-pointer text-blue hover:text-deepblue">
           관리자페이지
         </Link>
       )}
--- app/components/login/Login.tsx
+++ app/components/login/Login.tsx
@@ -8,32 +8,25 @@ import Profile from "../Profile";
 import ProfileSk from "../skeleton/ProfileSk";
 import toast from "react-hot-toast";
 import { refreshUser } from "@/app/api/authAction";
-import { useAuthStore } from "@/app/globalStatus/useAuthStore";
 import { useUserStore } from "@/app/globalStatus/useUserStore";
 import { UserInfo } from "@/app/types";
 
 const Login: React.FC = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-
   const [loading, setLoading] = useState(true);
-  const [userInfo, setUserInfoState] = useState<UserInfo | null>(null);
-  const { loggedIn, setLoggedIn } = useAuthStore();
-  const { setUserInfo, clearUserInfo } = useUserStore();
+  const { userInfo, setUserInfo, clearUserInfo } = useUserStore();
+
   useEffect(() => {
     const initializeUser = async () => {
       const data = await refreshUser();
-
       if (data != null) {
-        setLoggedIn(true);
         setUserInfo(data);
-        setUserInfoState(data);
       } else {
-        setLoggedIn(false);
+        clearUserInfo();
       }
       setLoading(false);
     };
-
     initializeUser();
   }, []);
 
@@ -41,21 +34,13 @@ const Login: React.FC = () => {
     try {
       const response = await fetch("/api/auth", {
         method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          username: username,
-          password: password,
-        }),
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, password }),
       });
 
       if (response.ok) {
-        setLoggedIn(true);
-        clearUserInfo();
         const { data } = await response.json();
         setUserInfo(data);
-        setUserInfoState(data);
       } else {
         toast.error("아이디와 비밀번호를 확인해주세요");
       }
@@ -70,7 +55,7 @@ const Login: React.FC = () => {
 
   return (
     <div className="max-w-128 bg-white p-8 rounded-lg w-full border-solid border-slate-200 border">
-      {loggedIn && userInfo ? (
+      {userInfo ? (
         <Profile userInfo={userInfo} />
       ) : (
         <form
@@ -81,7 +66,7 @@ const Login: React.FC = () => {
         >
           <div className="relative mb-4">
             <input
-              type="string"
+              type="text"
               id="username"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
@@ -89,13 +74,7 @@ const Login: React.FC = () => {
               placeholder="아이디"
               required
             />
-            <Image
-              src={IdIcon}
-              width={17}
-              height={17}
-              alt="Id Icon"
-              className="absolute top-2 left-2"
-            />
+            <Image src={IdIcon} width={17} height={17} alt="Id Icon" className="absolute top-2 left-2" />
           </div>
           <div className="relative mb-4">
             <input
@@ -107,33 +86,23 @@ const Login: React.FC = () => {
               placeholder="비밀번호"
               required
             />
-            <Image
-              src={PassIcon}
-              width={17}
-              height={17}
-              alt="Password Icon"
-              className="absolute top-2 left-2"
-            />
+            <Image src={PassIcon} width={17} height={17} alt="Password Icon" className="absolute top-2 left-2" />
           </div>
           <section className="flex flex-col gap-3">
-            <div className="flex flex-col gap-2 items-center justify-between">
+            <button
+              type="submit"
+              className="py-3 px-4 bg-blue hover:bg-[#2250f5] text-white font-bold w-full rounded focus:outline-none"
+            >
+              로그인
+            </button>
+            <Link href="/signup" className="w-full">
               <button
-                type="submit"
-                className="py-3 px-4 bg-blue hover:bg-[#2250f5] text-white font-bold w-full rounded focus:outline-none"
+                type="button"
+                className="py-3 px-4 bg-[#6870e9] hover:bg-[#525dee] text-white font-bold w-full rounded focus:outline-none"
               >
-                로그인
+                회원가입
               </button>
-            </div>
-            <div className="flex flex-col gap-2 items-center justify-between">
-              <Link className="w-full" href={"/signup"}>
-                <button
-                  type="button"
-                  className="py-3 px-4 bg-[#6870e9] hover:bg-[#525dee] text-white font-bold w-full rounded focus:outline-none"
-                >
-                  회원가입
-                </button>
-              </Link>
-            </div>
+            </Link>
           </section>
         </form>
       )}
