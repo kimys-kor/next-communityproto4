29ed7fd22a3a4cd54b0a7d9776e8c735d84f2cf7
kimys <ys2life1@naver.com>
2024-11-14 16:51:13 +0900

    로봇수정

--- app/robots.ts
+++ app/robots.ts
@@ -1,14 +1,14 @@
-import { MetadataRoute } from 'next'
- 
+import { MetadataRoute } from "next";
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/private/',
+      userAgent: "*",
+      allow: "/",
+      disallow: "/private/",
     },
-    sitemap: 'https://example.com/sitemap.xml',
-  }
+    sitemap: "https://ggongpang.com/sitemap.xml",
+  };
 }
 
 /*
