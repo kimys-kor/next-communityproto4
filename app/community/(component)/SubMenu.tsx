25af2c7cf4bedf23cb38fd76152dadb54442d611
kimys <ys2life1@naver.com>
2025-02-23 15:29:28 +0900

    서브메뉴 복권

--- app/community/(component)/SubMenu.tsx
+++ app/community/(component)/SubMenu.tsx
@@ -1,32 +1,17 @@
+import photo from "/public/images/icon/photoIgalIcon.png";
+import humor from "/public/images/icon/humor.png";
+import analyze from "/public/images/icon/analyze.png";
+import reasearch from "/public/images/icon/reasearch.png";
+import freeIcon from "/public/images/icon/freeIcon.png";
 import SubMenuList from "@/app/components/SubMenuList";
 
 function SubMenu() {
   const menuItems = [
-    {
-      href: "/community",
-      label: "안구정화",
-      icon: "/images/icon/photoIgalIcon.png",
-    },
-    {
-      href: "/community/humor",
-      label: "유머&이슈",
-      icon: "/images/icon/humor.png",
-    },
-    {
-      href: "/community/pickster",
-      label: "나는분석왕",
-      icon: "/images/icon/analyze.png",
-    },
-    {
-      href: "/community/free",
-      label: "자유게시판",
-      icon: "/images/icon/freeIcon.png",
-    },
-    {
-      href: "/community/case",
-      label: "피해사례",
-      icon: "/images/icon/reasearch.png",
-    },
+    { href: "/community", label: "안구정화", icon: photo },
+    { href: "/community/humor", label: "유머&이슈", icon: humor },
+    { href: "/community/pickster", label: "나는분석왕", icon: analyze },
+    { href: "/community/free", label: "자유게시판", icon: freeIcon },
+    { href: "/community/case", label: "피해사례", icon: reasearch },
   ];
 
   return <SubMenuList menuItems={menuItems} />;
--- app/components/SubMenuList.tsx
+++ app/components/SubMenuList.tsx
@@ -1,11 +1,12 @@
 "use client";
 
+import Image, { StaticImageData } from "next/image";
 import Link from "next/link";
 
 interface MenuItem {
   href: string;
   label: string;
-  icon: string; // Changed to string to accommodate the path of image
+  icon: StaticImageData;
 }
 
 interface SubMenuListProps {
@@ -22,7 +23,7 @@ const SubMenuList: React.FC<SubMenuListProps> = ({ menuItems }) => {
             className="flex items-center gap-2 sm:w-auto py-2 transform transition-transform duration-200 px-2 hover:bg-gray-100 rounded-lg"
           >
             <Link href={item.href} className="flex items-center gap-2">
-              <img src={item.icon} width={18} height={18} alt="menuIcon" />
+              <Image src={item.icon} width={18} height={18} alt="menuIcon" />
               <p className="text-sm sm:text-base">{item.label}</p>
             </Link>
           </div>
