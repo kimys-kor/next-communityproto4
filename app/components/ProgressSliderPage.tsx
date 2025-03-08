9f01c6496c59d146448e5c962157770df5df63a8
kimys <ys2life1@naver.com>
2025-03-08 16:28:02 +0900

    메인 배너 수정

--- app/components/ProgressSliderPage.tsx
+++ app/components/ProgressSliderPage.tsx
@@ -1,5 +1,6 @@
 import main_top from "/public/images/main_top.png";
 import ProgressSlider from "./ProgressSlider";
+import Image from "next/image";
 
 export default function ProgressSliderPage() {
   const items = [
@@ -17,7 +18,8 @@ export default function ProgressSliderPage() {
     <div className="relative h-32 md:h-48 flex flex-col justify-center bg-transparent overflow-hidden">
       <div className="max-w-6xl mx-auto py-3">
         <div className="mt-3 flex justify-center bg-[#F2F7FF]">
-          <ProgressSlider items={items} />
+          {/* <ProgressSlider items={items} /> */}
+          <Image src={main_top} width={1024} height={270} alt={"토이소 배너"} />
         </div>
       </div>
     </div>
